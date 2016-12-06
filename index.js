260e6f9d487a553fb9a2ca161451ae3769418f26
fidelscodes <fidelscodes@gmail.com>
2016-12-06 14:41:42 -0500

    Done

---
+++ index.js
@@ -0,0 +1,17 @@
+function countdown(callback) { 
+  setTimeout(function(){ callback(); }, 2000);
+}
+
+function createMultiplier(multiplierValue) {
+  return function(value) { return value * multiplierValue; };
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+
+function multiplier(multiplierValue, value) {
+  return multiplierValue * value;
+}
+
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
